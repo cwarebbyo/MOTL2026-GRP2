ba54ad078bf485c97f57935760fa7327763ad6d8
cwarebbyo <143715409+cwarebbyo@users.noreply.github.com>
2026-04-24 20:54:04 -0500

    Update layout.tsx

--- motl-memory-book/app/layout.tsx
+++ motl-memory-book/app/layout.tsx
@@ -1,8 +1,11 @@
+import { Analytics } from "@vercel/analytics/next"
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body style={{ fontFamily: 'sans-serif', padding: 20 }}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
